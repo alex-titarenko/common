3663fe469f81f8b3f540e9928b22aa9598f45af9
Alex Titarenko <alex.titarenko@live.com>
2015-02-22 22:17:26 +0200

    ObjectExtensions class moved to TAlex.Common project.

---
+++ TAlex.Common.Tests/Extensions/ObjectExtensionsTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using NUnit.Framework;
+using TAlex.Common.Extensions;
+
+
+namespace TAlex.Common.Test.Extensions
+{
+    [TestFixture]
+    public class ObjectExtensionsTest
+    {
+        #region IsNull
+
+        [Test]
+        public void IsNull_NullObject_True()
+        {
+            //arrange
+            object target = null;
+
+            //action
+            var actual = target.IsNull();
+
+            //assert
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void IsNull_NotNullObject_False()
+        {
+            //arrange
+            object target = "Test";
+
+            //action
+            var actual = target.IsNull();
+
+            //assert
+            Assert.IsFalse(actual);
+        }
+
+        #endregion
+
+        #region IsNotNull
+
+        [Test]
+        public void IsNotNull_NotNullObject_True()
+        {
+            //arrange
+            object target = "Test";
+
+            //action
+            var actual = target.IsNotNull();
+
+            //assert
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void IsNotNull_NullObject_False()
+        {
+            //arrange
+            object target = null;
+
+            //action
+            var actual = target.IsNotNull();
+
+            //assert
+            Assert.IsFalse(actual);
+        }
+
+        #endregion
+    }
+}
