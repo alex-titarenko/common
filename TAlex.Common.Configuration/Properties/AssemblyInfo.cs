ce2189cf170f651fea7cd3a99b88c081cc0f6b4e
Alex Titarenko <alex.titarenko@live.com>
2015-03-04 21:51:45 +0200

    add description to assembly info.

--- TAlex.Common.Configuration/Properties/AssemblyInfo.cs
+++ TAlex.Common.Configuration/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TAlex.Common.Configuration")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Configuration Providers Library.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("T-Alex Software")]
 [assembly: AssemblyProduct("TAlex.Common.Configuration")]
