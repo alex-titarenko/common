ce9bed49bdc401f5796f5ca084660cd2eebc901c
Alex Titarenko <alx.titarenko@gmail.com>
2018-04-07 11:04:40 -0700

    add AsyncHelper

---
+++ src/TAlex.Common/Helpers/AsyncHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace TAlex.Common.Helpers
+{
+    public static class AsyncHelper
+    {
+        public static T GetSyncResult<T>(this Func<Task<T>> func)
+        {
+            var syncTask = Task.Run(async () => { return await func(); });
+            syncTask.Wait();
+            return syncTask.Result;
+        }
+    }
+}
